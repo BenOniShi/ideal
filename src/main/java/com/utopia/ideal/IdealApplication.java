2653b485ffb923fa87b691007dfdb4778e173a4d
yearcold <shibn9510@gmail.com>
2020-05-16 00:08:16 +0800

    Initial commit

---
+++ src/main/java/com/utopia/ideal/IdealApplication.java
@@ -0,0 +1,13 @@
+package com.utopia.ideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdealApplication.class, args);
+    }
+
+}
